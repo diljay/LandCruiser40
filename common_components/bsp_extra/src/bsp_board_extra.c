a82e85ef867657d49d77dd6c4085fb8f738df23d
uidp1923 <dilhan.jayasuriya@continental-corporation.com>
2026-07-28 10:22:12 +0800

    Upgrade project to ESP-IDF 6.0.2 compatibility
    - Replace removed legacy driver/i2c.h with driver/i2c_master.h in bsp_extra
    - Widen registry component version bounds (esp_codec_dev, esp_lcd_ek79007, esp_lcd_ili9881c, esp-file-iterator) to allow IDF 6.0-compatible releases
    - Bump cmake_minimum_required 3.5 -> 3.16
    - Update README and sdkconfig.defaults IDF version references to 6.0.2

--- common_components/bsp_extra/src/bsp_board_extra.c
+++ common_components/bsp_extra/src/bsp_board_extra.c
@@ -13,7 +13,7 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_vfs_fat.h"
-#include "driver/i2c.h"
+#include "driver/i2c_master.h"
 #include "driver/i2s_std.h"
 #include "driver/gpio.h"
 #include "driver/ledc.h"
